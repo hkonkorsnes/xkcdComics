7fcfb67f48cb206c8f7cc54715aabbdf17c61862
hkonkorsnes <56083466+hkonkorsnes@users.noreply.github.com>
2022-04-06 23:37:32 +0200

    Initial Commit

---
+++ xkcdComics/xkcdComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdComicsApp.swift
+//  xkcdComics
+//
+//  Created by Håkon Korsnes on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct xkcdComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
