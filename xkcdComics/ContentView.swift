3d62a80aa77b508f178a448cbd7226d5441fb7ad
hkonkorsnes <hkonkorsnes@gmail.com>
2022-04-07 12:08:45 +0200

    Added and styled forward, previous and favorite buttons

--- xkcdComics/ContentView.swift
+++ xkcdComics/ContentView.swift
@@ -11,10 +11,42 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             VStack{
-            Image("testComic")
-                .resizable()
-                .frame(maxWidth: 500)
-                .navigationTitle("xkcdComics")
+                
+                Image("testComic")
+                    .resizable()
+                    .frame(maxWidth: 500)
+                    .navigationTitle("xkcdComics")
+                
+                HStack{
+                    Button(action: {
+                        print("Previous button tapped!")
+                    }) {
+                        Image(systemName: "chevron.left")
+                    }
+                        .buttonStyle(.bordered)
+                        .buttonBorderShape(.capsule)
+                        .tint(.blue)
+                        .font(.largeTitle)
+                    
+                    Button(action: {
+                        print("Favourite button tapped!")
+                    }) {
+                        Image(systemName: "heart")
+                    }
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                    .tint(.red)
+                    .font(.largeTitle)
+                    Button(action: {
+                        print("Next button tapped!")
+                    }) {
+                        Image(systemName: "chevron.right")
+                    }
+                        .buttonStyle(.bordered)
+                        .buttonBorderShape(.capsule)
+                        .tint(.green)
+                        .font(.largeTitle)
+                }
             }
         }
     }
@@ -23,5 +55,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
