621612b6c8dba971b61e44f1b7e155c081bde0dc
hkonkorsnes <56083466+hkonkorsnes@users.noreply.github.com>
2022-04-11 18:13:04 +0200

    Updated SheetView to use data from actual comic

--- xkcdComics/SheetView.swift
+++ xkcdComics/SheetView.swift
@@ -2,30 +2,22 @@
 //  SheetView.swift
 //  xkcdComics
 //
-//  Created by Louise Ellefsen on 07/04/2022.
+//  Created by Håkon Korsnes on 07/04/2022.
 //
 
 import SwiftUI
 
 struct SheetView: View {
-    @Environment(\.dismiss) var dismiss
+    
+    var comic: Comic?
     
     var body: some View {
-        ZStack{
+        ZStack {
             VStack{
-                HStack{
-                    Button(action: {
-                        dismiss()
-                    }) {
-                        Image(systemName: "x.circle")
-                            .font(.title2)
-                            .padding(10)
-                    }
-                    Spacer()
-                }
+                TopBar()
+                InfoView(comic: comic)
                 Spacer()
             }
-            Text("Comic info")
         }
     }
 }
@@ -33,5 +25,69 @@ struct SheetView: View {
 struct SheetView_Previews: PreviewProvider {
     static var previews: some View {
         SheetView()
+    }
+}
+
+struct TopBar: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        HStack{
+            Button(action: {
+                dismiss()
+                
+            }) {
+                Image(systemName: "x.circle")
+                    .font(.title2)
+            }
+                .frame(width: 44, height: 44)
+                .padding(10)
+            Spacer()
+        }
+    }
+}
+
+struct InfoView: View {
+    var comic: Comic?
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            if let comicTitle = comic?.title {
+                InfoCell(titleText: "Title", value: comicTitle)
+            }
+            
+            if let comicNumber = comic?.num {
+                InfoCell(titleText: "Number", value: "#\(comicNumber)")
+            }
+            
+            if let day = comic?.day, let month = comic?.month, let year = comic?.year {
+                InfoCell(titleText: "Release date", value: "\(day).\(month).\(year)")
+            }
+        }
+    }
+}
+
+struct InfoCell: View {
+    
+    var titleText: String
+    var value: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(titleText)
+                    .foregroundColor(.secondary)
+                    .font(.subheadline)
+                Spacer()
+                Text(value)
+                    .foregroundColor(.primary)
+                    .font(.subheadline)
+            }
+            Rectangle()
+                .frame(width: nil, height: 1)
+                .foregroundColor(.secondary)
+                .opacity(0.5)
+        }
+        .padding(.horizontal)
     }
 }
